9102d1e1155601e9604c3237f476c2b4d74de7cf
itcswld <itcswld@gmail.com>
2023-03-30 01:12:14 +0800

    add PK

--- Ch07_CREATE TABLE/Ch7_3_2(PrimaryKey).sql
+++ Ch07_CREATE TABLE/Ch7_3_2(PrimaryKey).sql
@@ -1,4 +1,4 @@
-USE �аȨt�� 
+USE dbName 
 GO
 CREATE TABLE tableName (
    col1   int       NOT NULL, 
@@ -7,5 +7,14 @@ CREATE TABLE tableName (
    PRIMARY KEY (col1, col2) --括號內為多欄位的複合鍵
 )
 
+--add primary key
+alter table <tbName> drop CONSTRAINT <constraint_name>
+alter table <tbName> add primary key (col1,col2,col3)
+
+--edit:you can find the constraint name by using the query below:
+select OBJECT_NAME(OBJECT_ID) AS NameofConstraint
+FROM sys.objects
+where OBJECT_NAME(parent_object_id)='tbName'
+and type_desc LIKE '%CONSTRAINT'
 
 
