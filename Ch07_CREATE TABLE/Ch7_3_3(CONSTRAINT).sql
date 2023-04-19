f7daf3ab27829bea3f106f497eff454c41de6d6c
itcswld <itcswld@gmail.com>
2023-04-20 00:16:38 +0800

    unique col allow nulls

--- Ch07_CREATE TABLE/Ch7_3_3(CONSTRAINT).sql
+++ Ch07_CREATE TABLE/Ch7_3_3(CONSTRAINT).sql
@@ -3,7 +3,7 @@ GO
 CREATE TABLE tbName (
    col1   int   NOT NULL IDENTITY PRIMARY KEY, 
    col2  money NOT NULL 
-       CONSTRAINT col2_constrant
+       CONSTRAINT ck_col2
        CHECK (col2 > 0),
    col3   money DEFAULT 0
        CHECK (col3 > 0)
@@ -11,6 +11,13 @@ CREATE TABLE tbName (
 
 --drop constraint
 alter table <tbName> drop CONSTRAINT <constraint_name>
+
+--add contrant
+alter table dbName 
+add constraint 
+df_col1 
+default([dbo].[fnCol1]())
+for col1
 
 
 
