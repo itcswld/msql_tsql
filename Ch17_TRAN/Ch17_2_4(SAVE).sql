ed181492879b51d92e596796ec432026a2833727
itcswld <itcswld@gmail.com>
2023-06-18 22:33:11 +0800

    save point

--- Ch17_TRAN/Ch17_2_4(SAVE).sql
+++ Ch17_TRAN/Ch17_2_4(SAVE).sql
@@ -3,36 +3,30 @@ GO
 -- rollback 指定交易點
 
 BEGIN TRAN
-
 DECLARE @ct INT
-
-SELECT @ct = COUNT(*) FROM classDup 
-PRINT 'classDup rows: ' + CONVERT(VARCHAR, @ct) --12
-DELETE classDup WHERE stud_no = 'S002'
-SELECT @ct = COUNT(*) FROM classDup 
-PRINT 'classDup rows: ' + CONVERT(VARCHAR, @ct) --9
-
-SELECT @ct = COUNT(*) FROM studDup
-PRINT 'studDup rows: ' + CONVERT(VARCHAR, @ct) --8
-
-SAVE TRAN sp1 --儲存交易點
-DELETE studDup WHERE [no] = 'S002'
 SELECT @ct = COUNT(*) FROM studDup 
-PRINT 'studDup rows: ' + CONVERT(VARCHAR, @ct)  --7
+PRINT 'rows: ' + CONVERT(VARCHAR, @ct) --9
+SAVE TRAN sp1 --儲存交易點/交易儲存點
+    DELETE studDup WHERE no = 'S001'
+    SELECT @ct = COUNT(*) FROM studDup 
+    PRINT 'rows: ' + CONVERT(VARCHAR, @ct) --8
+    PRINT 'tran: ' + CONVERT(VARCHAR, @@trancount) --1
 
 SAVE TRAN sp2
-DELETE studDup WHERE [no] = 'S003'
-SELECT @ct = COUNT(*) FROM studDup
-PRINT 'studDup rows: ' + CONVERT(VARCHAR, @ct) -- 6
+    DELETE studDup WHERE [no] = 'S002'
+    SELECT @ct = COUNT(*) FROM studDup
+    PRINT 'rows: ' + CONVERT(VARCHAR, @ct) -- 7
+    PRINT 'tran: ' + CONVERT(VARCHAR, @@trancount) --1
 
-ROLLBACK TRAN sp1
+ROLLBACK TRAN sp2 -- 指定回復到sp2交易點
 SELECT @ct = COUNT(*) FROM studDup
-PRINT 'studDup rows: ' + CONVERT(VARCHAR, @ct) -- 7
+PRINT 'rows: ' + CONVERT(VARCHAR, @ct) -- 8
+PRINT 'tran: ' + CONVERT(VARCHAR, @@trancount)
 
-ROLLBACK TRAN sp2 -- rollback 指定儲存交易點
+ROLLBACK TRAN sp1 -- 再回復到sp2交易點
 SELECT @ct = COUNT(*) FROM studDup
-PRINT 'studDup rows: ' + CONVERT(VARCHAR, @ct) -- 8
-
+PRINT 'rows: ' + CONVERT(VARCHAR, @ct) -- 9
+PRINT 'tran: ' + CONVERT(VARCHAR, @@trancount)
 
 COMMIT TRAN
 
